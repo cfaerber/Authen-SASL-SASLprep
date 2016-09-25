06430c642a2a7d928085339741bf5b05b919db6f
Claus Faerber <cfaerber@cfaerber.name>
2016-09-25 15:19:46 +0200

    implement stored strings, new version 1.100

--- lib/Authen/SASL/SASLprep.pm
+++ lib/Authen/SASL/SASLprep.pm
@@ -5,7 +5,7 @@ use utf8;
 use warnings;
 require 5.006_000;
 
-our $VERSION = '1.011';
+our $VERSION = '1.100';
 $VERSION = eval $VERSION;
 
 require Exporter;
@@ -28,7 +28,22 @@ for(my $pos=0; $pos <= $#Unicode::Stringprep::Prohibited::C12; $pos+=2)
   }
 };
 
-*saslprep = Unicode::Stringprep->new(
+our $_saslprep_stored;
+our $_saslprep_query;
+
+sub saslprep {
+  my ($input, $stored) = @_;
+  splice @_, 1, 1; ## remove $stored
+
+  if($stored) {
+    goto &$_saslprep_stored;
+  } else {
+    goto &$_saslprep_query;
+  }
+}
+
+BEGIN {
+  my @_common_args = (
   3.2,
   [ \@Unicode::Stringprep::Mapping::B1,
     \%C12_to_SPACE ],
@@ -45,7 +60,18 @@ for(my $pos=0; $pos <= $#Unicode::Stringprep::Prohibited::C12; $pos+=2)
     \@Unicode::Stringprep::Prohibited::C9,
   ],
   1
-);
+  );
+
+  our $_saslprep_stored = Unicode::Stringprep->new(
+    @_common_args,
+    1,
+  );
+
+  our $_saslprep_query = Unicode::Stringprep->new(
+    @_common_args,
+    0,
+  );
+};
 
 1;
 __END__
@@ -59,7 +85,8 @@ Authen::SASL::SASLprep - A Stringprep Profile for User Names and Passwords (RFC 
 =head1 SYNOPSIS
 
   use Authen::SASL::SASLprep;
-  $output = saslprep $input;
+  $output_query = saslprep $input;
+  $output_stored = saslprep $stored;
 
 =head1 DESCRIPTION
 
@@ -73,7 +100,7 @@ This module implements a single function, C<saslprep>, which is exported by defa
 
 =over 4
 
-=item B<saslprep($input)>
+=item B<saslprep($input, [$stored]>
 
 Processes C<$input> according to the I<SASLprep> specification and
 returns the result.
@@ -81,7 +108,10 @@ returns the result.
 If C<$input> contains characters not allowed for I<SASLprep>, it
 throws an exception (so use C<eval> if necessary).
 
-This function currently supports preparation for I<query> strings only.
+If the boolean parameter C<$stored> is true, an exception is also thrown when
+characters are not allowed for stored strings (i.e., when characters are
+unassigned in Unicode 3.2). The default is to prepare query strings, in which
+unassigned characters are allowed.
 
 =back
 
@@ -91,7 +121,7 @@ Claus Färber <CFAERBER@cpan.org>
 
 =head1 LICENSE
 
-Copyright 2009-2015 Claus Färber.
+Copyright 2009-2016 Claus Färber.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
--- t/saslprep.t
+++ t/saslprep.t
@@ -31,13 +31,28 @@ our @strprep = (
   [ "\x{3000}",		' ',		'ideographic space mapped to ASCII space' ],
   [ "\x{A0}\x{2009}\x{3000}", '   ',	'no space collapsing' ],
 
+  # newly assigned in Unicode 3.2
+  #
+  [ "\x{2047}", 	"??",		'Double question mark (added in Unicode 3.2)' ],
+  [ "\x{30FF}", 	"コト",		'Katakana digraph koto (added in Unicode 3.2)' ],
+  [ "M&\x{20B0}", 	"M&\x{20B0}",	'German penny sign (added in Unicode 3.2)' ],
+
+  # unassigned in 3.2
+  #
+  [ "I\x{0221}", 	"I\x{0221}",	'Latin small letter d with curl (added in Unicode 4.0)', 1 ],
+  [ "\x{33FF}", 	"\x{33FF}",	'Square gal (added in Unicode 4.0, would decompose)', 1 ],
+  [ "\x{1F23B}", 	"\x{1F23B}",	'Squared CJK unified (added in Unicode 9.0, would decompose)', 1 ],
+  [ "\x{E01F8}", 	"\x{E01F8}",	'U+E01F8 (unassigned as of Unicode 9.0)', 1 ],
 );
 
-plan tests => ($#strprep+1) + 1;
+plan tests => ($#strprep+1)*3 + 1;
 
 foreach my $test (@strprep) 
 {
-  my ($in,$out,$comment) = @{$test};
+  my ($in,$out,$comment,$has_unassigned) = @{$test};
 
   is(eval{saslprep($in)}, $out, $comment);
+  is(eval{saslprep($in,0)}, $out, $comment.' (query)');
+  $out = undef if $has_unassigned;
+  is(eval{saslprep($in,1)}, $out, $comment.' (stored)');
 }
